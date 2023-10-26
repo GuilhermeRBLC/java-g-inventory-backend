db276f7af76886a99c3433645d9ebc135fd9dd69
Guilherme <guilhermerblc@gmail.com>
2023-10-26 20:15:01 -0300

    Creating the repositories interfaces.

---
+++ src/main/java/com/guilhermerblc/inventory/repository/ProductRepository.java
@@ -0,0 +1,10 @@
+package com.guilhermerblc.inventory.repository;
+
+import com.guilhermerblc.inventory.models.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProductRepository extends JpaRepository<Product, Long> {
+
+}
---
+++ src/main/java/com/guilhermerblc/inventory/repository/ReportRepository.java
@@ -0,0 +1,10 @@
+package com.guilhermerblc.inventory.repository;
+
+import com.guilhermerblc.inventory.models.Report;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ReportRepository extends JpaRepository<Report, Long> {
+
+}
