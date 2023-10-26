73d2f4029eeae8cbbf48f3c189f5025eadbe74f4
Guilherme <guilhermerblc@gmail.com>
2023-10-26 18:34:25 -0300

    Initializing the project.

---
+++ src/main/java/com/guilhermerblc/inventory/GInventoryApplication.java
@@ -0,0 +1,13 @@
+package com.guilhermerblc.inventory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GInventoryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GInventoryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/guilhermerblc/inventory/GInventoryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.guilhermerblc.inventory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GInventoryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
