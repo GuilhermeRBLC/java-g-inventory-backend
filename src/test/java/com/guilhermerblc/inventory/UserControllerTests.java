33fc1120815bb454d23b977202b5ba136195aa68
Guilherme <guilhermerblc@gmail.com>
2023-11-14 19:36:53 -0300

    Fail tests for User controller.

--- src/test/java/com/guilhermerblc/inventory/UserControllerTests.java
+++ src/test/java/com/guilhermerblc/inventory/UserControllerTests.java
@@ -33,20 +33,10 @@ public class UserControllerTests {
     @Autowired
     private TestRestTemplate restTemplate;
 
+    @Autowired
+    private AuthenticationHelper autenticationHelper;
+
     private final String urlPath = "/api/v1/user";
-
-    String authenticate() throws Exception {
-        String signingUrl = "http://localhost:" + port + "/api/v1/auth/signing";
-
-        SigningRequest signingRequest = new SigningRequest("gerente", "1234");
-
-        ResponseEntity<JwtAuthenticationResponse> responseAuth = restTemplate.postForEntity(signingUrl, signingRequest, JwtAuthenticationResponse.class);
-
-        assertThat(responseAuth.getStatusCode()).isEqualTo(HttpStatus.OK);
-        assertThat(responseAuth.getBody()).isNotNull();
-
-        return responseAuth.getBody().getToken();
-    }
 
     @Test
     void gerenteUserShouldExist() throws Exception {
@@ -82,7 +72,7 @@ public class UserControllerTests {
                 null
         );
 
-        String authenticationToken = authenticate();
+        String authenticationToken = autenticationHelper.authenticate(port, restTemplate);
 
         // Act
         restTemplate.getRestTemplate().setInterceptors(Collections.singletonList(((request, body, execution) -> {
@@ -137,7 +127,7 @@ public class UserControllerTests {
 
         String requestUrl = "http://localhost:" + port + urlPath + "/" + requestObject.getId();
 
-        String authenticationToken = authenticate();
+        String authenticationToken = autenticationHelper.authenticate(port, restTemplate);
 
         // Act
 
@@ -147,12 +137,12 @@ public class UserControllerTests {
         headers.setBearerAuth(authenticationToken);
 
         HttpEntity<User> httpEntity = new HttpEntity<>(requestObject, headers);
-        ResponseEntity<Object> response = restTemplate.exchange(requestUrl, HttpMethod.PUT, httpEntity, Object.class);
+        ResponseEntity<User> response = restTemplate.exchange(requestUrl, HttpMethod.PUT, httpEntity, User.class);
 
         // Assert
         assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
         assertThat(response.getBody()).isNotNull();
-        /*assertThat(response.getBody().getName()).isEqualTo(requestObject.getName());
+        assertThat(response.getBody().getName()).isEqualTo(requestObject.getName());
         assertThat(response.getBody().getRole()).isEqualTo(requestObject.getRole());
         assertThat(response.getBody().getUsername()).isEqualTo(requestObject.getUsername());
         assertThat(response.getBody().getPassword()).isNotNull();
@@ -163,7 +153,7 @@ public class UserControllerTests {
         )).isTrue();
 
         assertThat(response.getBody().getCreated()).isNotNull();
-        assertThat(response.getBody().getModified()).isNotNull();*/
+        assertThat(response.getBody().getModified()).isNotNull();
     }
 
     @Test
@@ -190,7 +180,7 @@ public class UserControllerTests {
 
         String requestUrl = "http://localhost:" + port + urlPath;
 
-        String authenticationToken = authenticate();
+        String authenticationToken = autenticationHelper.authenticate(port, restTemplate);
 
         // Act
         restTemplate.getRestTemplate().setInterceptors(Collections.singletonList(((request, body, execution) -> {
@@ -210,15 +200,21 @@ public class UserControllerTests {
     void userShouldGetOne() throws Exception {
         // Arrange
 
+        List<User> reports = userRepository.findAll();
+        Optional<User> greatestIdUser = reports.stream().max(Comparator.comparing(User::getId));
+        long greatestId = 1L;
+        if(greatestIdUser.isPresent()) greatestId = greatestIdUser.get().getId() + 1;
+
+
         List<Permission> permissionsList = permissionRepository.findAll();
 
         List<User> userList = new ArrayList<>(userRepository.findAll());
         for (int i = 0; i < 10; i++) {
             userList.add(new User(
                     null,
-                    "Novo Usuário",
+                    "Novo Usuário " + (i + greatestId),
                     "Estoquista",
-                    "novo,estoquista" + i,
+                    "novo,estoquista" + (i + greatestId),
                     "987654",
                     Status.ACTIVE,
                     permissionsList,
@@ -231,7 +227,7 @@ public class UserControllerTests {
         User requestObject = userList.get(5);
         String requestUrl = "http://localhost:" + port + urlPath + "/" + requestObject.getId();
 
-        String authenticationToken = authenticate();
+        String authenticationToken = autenticationHelper.authenticate(port, restTemplate);
 
         // Act
         restTemplate.getRestTemplate().setInterceptors(Collections.singletonList(((request, body, execution) -> {
@@ -278,7 +274,7 @@ public class UserControllerTests {
 
         String productUrl = "http://localhost:" + port + urlPath + "/" + requestObject.getId();
 
-        String authenticationToken = authenticate();
+        String authenticationToken = autenticationHelper.authenticate(port, restTemplate);
 
         // Act
         HttpHeaders headers = new HttpHeaders();
@@ -301,6 +297,203 @@ public class UserControllerTests {
         assertThat(response.getBody()).isNull();
 
         assertThat(databaseUser.isPresent()).isFalse();
+    }
+
+    // Fail tests
+
+    @Test
+    void userShouldFailToGetOne_DueToInvalidID() throws Exception {
+        // Arrange
+
+        List<User> reports = userRepository.findAll();
+        Optional<User> greatestId = reports.stream().max(Comparator.comparing(User::getId));
+        long invalidId = 1L;
+        if(greatestId.isPresent()) invalidId = greatestId.get().getId() + 1;
+
+        String requestUrl = "http://localhost:" + port + urlPath + "/" + invalidId;
+        String authenticationToken = autenticationHelper.authenticate(port, restTemplate);
+
+        // Act
+        restTemplate.getRestTemplate().setInterceptors(Collections.singletonList(((request, body, execution) -> {
+            request.getHeaders().add("Authorization", "Bearer " + authenticationToken);
+            return execution.execute(request, body);
+        })));
+        ResponseEntity<Object> response = restTemplate.getForEntity(requestUrl, Object.class);
+
+        // Assert
+        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.NOT_FOUND);
+        assertThat(response.getBody()).isNotNull();
+        assertThat(response.getBody().toString().contains("Item not found.")).isTrue();
+    }
+
+    @Test
+    void userShouldNotUpdate_DueInvalidData() throws Exception {
+        // Arrange
+
+        List<User> reports = userRepository.findAll();
+        Optional<User> greatestIdUser = reports.stream().max(Comparator.comparing(User::getId));
+        long greatestId = 1L;
+        if(greatestIdUser.isPresent()) greatestId = greatestIdUser.get().getId() + 1;
+
+        List<Permission> permissionsList = permissionRepository.findAll();
+
+        List<User> users = new ArrayList<>();
+        for (int i = 0; i < 5; i++) {
+            users.add(new User(
+                    null,
+                    "Novo Usuário " + (greatestId + i),
+                    "Estoquista",
+                    "novo,estoquista" + (greatestId + i),
+                    "987654",
+                    Status.ACTIVE,
+                    permissionsList,
+                    LocalDateTime.now(),
+                    null
+            ));
+        }
+        users = userRepository.saveAll(users);
+
+        User user = users.get(0);
+
+        user.setName("Larger Than 125 characters".repeat(125));
+        user.setRole("Larger Than 125 characters".repeat(125));
+        user.setUsername("Larger Than 20 characters".repeat(20));
+
+        String requestUrl = "http://localhost:" + port + urlPath + "/" + user.getId();
+        String authenticationToken = autenticationHelper.authenticate(port, restTemplate);
+
+        // Act
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+        headers.setAccept(List.of(MediaType.APPLICATION_JSON));
+        headers.setBearerAuth(authenticationToken);
+
+        HttpEntity<User> httpEntity = new HttpEntity<>(user, headers);
+        ResponseEntity<Object> response = restTemplate.exchange(
+                requestUrl,
+                HttpMethod.PUT,
+                httpEntity,
+                Object.class
+        );
+
+        // Assert
+        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.BAD_REQUEST);
+        assertThat(response.getBody()).isNotNull();
+        assertThat(response.getBody().toString().contains("Invalid data.")).isTrue();
+    }
+
+    @Test
+    void userShouldNotUpdate_DueDifferentIDs() throws Exception {
+        // Arrange
+
+        List<User> reports = userRepository.findAll();
+        Optional<User> greatestIdUser = reports.stream().max(Comparator.comparing(User::getId));
+        long greatestId = 1L;
+        if(greatestIdUser.isPresent()) greatestId = greatestIdUser.get().getId() + 1;
+
+        List<Permission> permissionsList = permissionRepository.findAll();
+
+        List<User> users = new ArrayList<>();
+        for (int i = 0; i < 5; i++) {
+            users.add(new User(
+                    null,
+                    "Novo Usuário " + (greatestId + i),
+                    "Estoquista",
+                    "novo,estoquista" + (greatestId + i),
+                    "987654",
+                    Status.ACTIVE,
+                    permissionsList,
+                    LocalDateTime.now(),
+                    null
+            ));
+        }
+        users = userRepository.saveAll(users);
+
+        User user = users.get(0);
+
+        user.setName("Lower Than 125 characters");
+        user.setRole("Lower Than 125 characters");
+        user.setUsername("Lower Than 20 chars");
+
+        // The ID in path must be the same in object. It should cause a fail.
+        String requestUrl = "http://localhost:" + port + urlPath + "/" + (user.getId() + 1);
+        String authenticationToken = autenticationHelper.authenticate(port, restTemplate);
+
+        // Act
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+        headers.setAccept(List.of(MediaType.APPLICATION_JSON));
+        headers.setBearerAuth(authenticationToken);
+
+        HttpEntity<User> httpEntity = new HttpEntity<>(user, headers);
+        ResponseEntity<Object> response = restTemplate.exchange(
+                requestUrl,
+                HttpMethod.PUT,
+                httpEntity,
+                Object.class
+        );
+
+        // Assert
+        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.UNPROCESSABLE_ENTITY);
+        assertThat(response.getBody()).isNotNull();
+        assertThat(response.getBody().toString().contains("IDs must be the same in path and body.")).isTrue();
+    }
+
+    @Test
+    void userShouldNotUpdate_DueLackOfPermissionOfUser() throws Exception {
+        // Arrange
+
+        List<User> reports = userRepository.findAll();
+        Optional<User> greatestIdUser = reports.stream().max(Comparator.comparing(User::getId));
+        long greatestId = 1L;
+        if(greatestIdUser.isPresent()) greatestId = greatestIdUser.get().getId() + 1;
+
+        List<Permission> permissionsList = permissionRepository.findAll();
+
+        List<User> users = new ArrayList<>();
+        for (int i = 0; i < 5; i++) {
+            users.add(new User(
+                    null,
+                    "Novo Usuário " + (i + greatestId),
+                    "Estoquista",
+                    "novo,estoquista" + (i + greatestId),
+                    "987654",
+                    Status.ACTIVE,
+                    permissionsList,
+                    LocalDateTime.now(),
+                    null
+            ));
+        }
+        users = userRepository.saveAll(users);
+
+        User user = users.get(0);
+
+        user.setName("Lower Than 125 characters");
+        user.setRole("Lower Than 125 characters");
+        user.setUsername("Lower Than 20 chars");
+
+        String requestUrl = "http://localhost:" + port + urlPath + "/" + user.getId();
+
+        // The current user should have no permission to update this entity.
+        String authenticationToken = autenticationHelper.authenticateLowPermissions(port, restTemplate);
+
+        // Act
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+        headers.setAccept(List.of(MediaType.APPLICATION_JSON));
+        headers.setBearerAuth(authenticationToken);
+
+        HttpEntity<User> httpEntity = new HttpEntity<>(user, headers);
+        ResponseEntity<Object> response = restTemplate.exchange(
+                requestUrl,
+                HttpMethod.PUT,
+                httpEntity,
+                Object.class
+        );
+
+        // Assert
+        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.FORBIDDEN);
+        assertThat(response.getBody()).isNull();
     }
 
 }
